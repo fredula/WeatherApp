e49365699a4708df339e818ec40cd05b4038e01c
fredula <fred.thefriedone@gmail.com>
2017-03-15 09:33:49 +0200

    Added grid styling, changed example page markup

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
 	return(
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to the examples page</p>
+			<h1 className="text-center">Examples</h1>
+			<p>Find a few example locations</p>
+			<ol>
+				<li>
+					<Link to="/?location=Cape Town">Cape Town, South Africa</Link>
+				</li>
+				<li>
+					<Link to="/?location=Johannesburg">Johannesburg, South Africa</Link>
+				</li>
+			</ol>
 		</div>
 	)
 }
