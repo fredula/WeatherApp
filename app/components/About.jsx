49f79517a9a1ac9d32218350d50dd5ae77c8385a
fredula <fred.thefriedone@gmail.com>
2017-03-29 22:01:42 +0200

    styled the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page</p>
+			<h1 className="text-center">About</h1>
+			<p>
+				This is a simple weather app built with React.
+				It uses the  <a href="http://openweathermap.org/" target="_blank">openweather API</a>.
+			</p>
+			<p>
+				You can have a look ate some examples on the <Link to="/examples">examples page.</Link>
+				Alternatively, you can search for the weather in your town or city using the search in the navigation bar or on the <Link to="/">get weather page.</Link>
+			</p>
 		</div>
 	)
 }
