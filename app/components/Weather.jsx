78b3606d0e4bba018b860d03b371b1b63fed38e6
fredula <fred.thefriedone@gmail.com>
2017-04-03 21:58:13 +0200

    added erro

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
 	getInitialState: function() {
@@ -11,21 +12,28 @@ var Weather = React.createClass({
 	},
 	handleSearch: function(location) {
 		var that = this;
-		this.setState({isLoading: true});
+		this.setState({
+			isLoading: true,
+			errorMessage: undefined
+		});
 		openWeatherMap.getTemp(location).then(function(temp) {
 			that.setState({
 				location: location,
 				temp: temp,
 				isLoading: false
 			})
-		}, function(errorMessage) {
-			that.setState({isLoading: false});
-			alert(errorMessage);
+		}, function(e) {
+			that.setState({
+				isLoading: false,
+				errorMessage: e.message
+			});
+			console.log(e.message);
 		})
 		console.log(location)
 	},
 	render: function() {
-		var {isLoading, location, temp} = this.state;
+		var {isLoading, location, temp, errorMessage} = this.state;
+		console.log(this.state);
 
 		function renderMessage() {
 			if(isLoading) {
@@ -35,11 +43,20 @@ var Weather = React.createClass({
 			}
 		}
 
+		function renderError() {
+			if(typeof errorMessage === 'string') {
+				return (
+					<ErrorModal message={errorMessage}/>
+				)
+			}
+		}
+
 		return (
 			<div>
 				<h1 className="text-center">Get Weather</h1>
 				<WeatherForm onSearch={this.handleSearch}/>
 				{renderMessage()}
+				{renderError()}
 			</div>
 		)
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
 			WeatherMessage: 'app/components/WeatherMessage.jsx',
 			About: 'app/components/About.jsx',
 			Examples: 'app/components/Examples.jsx',
+			ErrorModal: 'app/components/ErrorModal.jsx',
 			openWeatherMap: 'app/api/openWeatherMap.jsx'
 		},
 		extensions: ['', '.js', '.jsx']
