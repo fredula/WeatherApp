be6a7397180952c1a7f8403a1595aeb457344be1
fredula <fred.thefriedone@gmail.com>
2017-04-09 22:41:40 +0200

    switched to scss

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 //custom css
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
 	<Router history={hashHistory}>
